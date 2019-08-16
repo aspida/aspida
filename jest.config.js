57a45a72da01a97447363a65ffacc504fd9c85e3
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-08-16 13:05:47 +0900

    chore: add configuration file of Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+};
