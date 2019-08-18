b797df0718af85d0fe0797b302c84b20cd45b3b8
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-08-18 19:29:49 +0900

    chore(lint): set up a configuration file of ESLint
    Log.
    
    ```
    axios-mock-server $ npx eslint --init
    ? How would you like to use ESLint? To check syntax, find problems, and enforce code style
    ? What type of modules does your project use? JavaScript modules (import/export)
    ? Which framework does your project use? None of these
    ? Where does your code run? Node
    ? How would you like to define a style for your project? Use a popular style guide
    ? Which style guide do you want to follow? Standard (https://github.com/standard/standard)
    ? What format do you want your config file to be in? JavaScript
    ```

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
