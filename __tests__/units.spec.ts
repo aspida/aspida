8aa7223d75f52b94f868649d73a2fddc9eaaef6d
m-mitsuhide <genbu2011@live.jp>
2019-09-07 00:25:24 +0900

    feat(toDataURI): delete from repository

--- __tests__/units.spec.ts
+++ __tests__/units.spec.ts
@@ -1,15 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-import { asyncResponse, toDataURI } from '~/src'
+import { asyncResponse } from '~/src'
 
 describe('unit tests', () => {
-  test('binary to dataURI', () => {
-    const imageBinary = fs.readFileSync(path.join(__dirname, './assets/image.png'))
-    const imageDataURI = fs.readFileSync(path.join(__dirname, './assets/image.b64'), 'utf-8')
-
-    expect(toDataURI(imageBinary)).toBe(imageDataURI)
-  })
-
   test('asyncResponse', async () => {
     const result = [200, { test: 'aaa' }, { 'cache-control': 'max-age=0' }] as const
     const res = await asyncResponse(result[0], (async () => result[1])(), result[2])
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { AxiosInstance } from 'axios'
 import { MockRoute, HttpMethod, MockResponse, MockMethods } from './types'
 import MockServer from './MockServer'
-import toDataURI from './toDataURI'
 import asyncResponse from './asyncResponse'
 
-export { toDataURI, asyncResponse, MockServer, MockRoute, HttpMethod, MockResponse, MockMethods }
+export { asyncResponse, MockServer, MockRoute, HttpMethod, MockResponse, MockMethods }
 
 export default (route?: MockRoute, client?: AxiosInstance) => new MockServer(route, client)
