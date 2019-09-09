618b74d084379764f57734516c17fb53dc8a9d32
m-mitsuhide <genbu2011@live.jp>
2019-09-10 01:53:29 +0900

    chore(test): add condition to createRelativePath

--- src/createRelativePath.ts
+++ src/createRelativePath.ts
@@ -1,6 +1,9 @@
 const regOriginURL = /^(https?:)?\/\/[^/]+/
+
 export default (url = '', baseURL?: string) =>
   (baseURL && !regOriginURL.test(url)
     ? `${baseURL.replace(/\/$/, '')}/${url.replace(/^\//, '')}`
     : url
-  ).replace(regOriginURL, '')
+  )
+    .replace(regOriginURL, '')
+    .replace(/\/$/, '')
