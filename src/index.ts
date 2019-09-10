7fe1c9edacff10498b9e25dd42bd2c5bfe87740c
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-09-10 13:59:03 +0900

    refactor: use require function without default in CommonJS

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,8 @@ import asyncResponse from './asyncResponse'
 export { asyncResponse, MockServer, MockRoute, HttpMethod, MockResponse, MockMethods }
 
 export default (route?: MockRoute, client?: AxiosInstance) => new MockServer(route, client)
+
+/* eslint-disable dot-notation */
+module.exports = exports['default']
+module.exports.default = exports['default']
+module.exports.asyncResponse = asyncResponse
