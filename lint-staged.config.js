75b12efdf6a6a704bcc2b1e1956a9d28e8d5dad7
m-mitsuhide <genbu2011@live.jp>
2019-12-30 05:44:59 +0900

    refactor: monorepo for aspida

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '**/*.ts': () => 'tsc --project tsconfig.json --noEmit',
-  '*.{js,ts}': ['eslint --fix --no-ignore', 'git add'],
-  '*.{json,md,yml}': ['prettier --write', 'git add']
-}
