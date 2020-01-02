3d100792a561cf3aa457da83e0db462c632d58ad
m-mitsuhide <genbu2011@live.jp>
2020-01-02 18:20:01 +0900

    refactor: use ts-morph

--- packages/aspida/src/createMethodsString.ts
+++ packages/aspida/src/createMethodsString.ts
@@ -1,127 +1,95 @@
-import * as ts from 'typescript'
+import { Project } from 'ts-morph'
+import { SyntaxKind } from 'typescript'
 
 type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'head' | 'patch'
+type MethodsProperties = 'data' | 'params' | 'headers' | 'response'
 
 export default (target: string, indent: string, importName: string, newUrl: string) => {
-  const program = ts.createProgram({
-    rootNames: [target],
-    options: {}
-  })
-
-  const source = program.getSourceFile(target)
+  const source = new Project().addSourceFileAtPath(target)
   const chanks: string[] = []
 
-  if (source) {
-    source.forEachChild(node => {
-      if (
-        ts.isInterfaceDeclaration(node) &&
-        node.name.escapedText === 'Methods' &&
-        node.modifiers &&
-        node.modifiers.length === 1 &&
-        ts.tokenToString(node.modifiers[0].kind) === 'export'
-      ) {
-        node.forEachChild(method => {
-          if (ts.isPropertySignature(method)) {
-            const methodName = (method.name as ts.Identifier).escapedText
-            // if (!(method.type as ts.TypeLiteralNode).members) {
-            //   console.log((method.type as ts.TypeReferenceNode).typeName)
-            //   return
-            // }
+  const methodsInterface = source.getInterface('Methods')
+  if (!methodsInterface) return ''
 
-            const typeInfo = (method.type as ts.TypeLiteralNode).members.reduce(
-              (prev, member) => {
-                return {
-                  ...prev,
-                  [(member.name as ts.Identifier).escapedText as string]: {
-                    hasQuestion: !!member.questionToken
-                  }
-                }
-              },
-              {} as {
-                [key in 'data' | 'params' | 'headers' | 'response']?: { hasQuestion: boolean }
-              }
-            )
-            const tmpChanks: string[] = []
-            const hasNotDataQuestion = typeInfo.data && !typeInfo.data.hasQuestion
-            const isConfigRequired = typeInfo.params && !typeInfo.params.hasQuestion
+  methodsInterface.getProperties().forEach(method => {
+    const methodName = method.getName()
+    const typeInfo: { [key in MethodsProperties]?: { hasQuestion: boolean } } = {}
+    method.getChildrenOfKind(SyntaxKind.TypeLiteral).forEach(n =>
+      n.getProperties().forEach(p => {
+        typeInfo[p.getName() as MethodsProperties] = { hasQuestion: p.hasQuestionToken() }
+      })
+    )
 
-            const data = (method: HttpMethod) =>
-              `data${`${hasNotDataQuestion || isConfigRequired ? '' : '?'}: ${
-                typeInfo.data
-                  ? `${importName}['${method}']['data']${
-                      typeInfo.data.hasQuestion ? ' | null' : ''
-                    }`
-                  : 'null'
-              }`}`
-            const params = (method: HttpMethod) =>
-              typeInfo.params
-                ? `{ params${
-                    typeInfo.params.hasQuestion ? '?' : ''
-                  }: ${importName}['${method}']['params'] & { [key: string]: any }} & `
-                : ''
-            const headers = (method: HttpMethod) =>
-              typeInfo.headers
-                ? `{ headers?: ${importName}['${method}']['headers'] & { [key: string]: any }} & `
-                : ''
-            const config = (method: HttpMethod) =>
-              `config${
-                isConfigRequired || (method === 'delete' && hasNotDataQuestion) ? '' : '?'
-              }: ${params(method)}${headers(method)}AxiosRequestConfig`
-            const response = (method: HttpMethod) =>
-              `${method}${
-                typeInfo.response ? `<${importName}['${method}']['response']>` : '<void>'
-              }`
+    const tmpChanks: string[] = []
+    const hasNotDataQuestion = typeInfo.data && !typeInfo.data.hasQuestion
+    const isConfigRequired = typeInfo.params && !typeInfo.params.hasQuestion
 
-            switch (methodName) {
-              case 'get':
-                tmpChanks.push(
-                  `(${config('get')}) =>`,
-                  `client.${response('get')}(\`${newUrl}\`, config)`
-                )
-                break
-              case 'head':
-                tmpChanks.push(
-                  `(${config('head')}) =>`,
-                  `client.${response('head')}(\`${newUrl}\`, config)`
-                )
-                break
-              case 'post':
-                tmpChanks.push(
-                  `(${data('post')}, ${config('post')}) =>`,
-                  `client.${response('post')}(\`${newUrl}\`, data, config)`
-                )
-                break
-              case 'put':
-                tmpChanks.push(
-                  `(${data('put')}, ${config('put')}) =>`,
-                  `client.${response('put')}(\`${newUrl}\`, data, config)`
-                )
-                break
-              case 'patch':
-                tmpChanks.push(
-                  `(${data('patch')}, ${config('patch')}) =>`,
-                  `client.${response('patch')}(\`${newUrl}\`, data, config)`
-                )
-                break
-              case 'delete':
-                tmpChanks.push(
-                  `(${config('delete')}${typeInfo.data ? ` & { ${data('delete')} }` : ''}) =>`,
-                  `client.${response('delete')}(\`${newUrl}\`, config)`
-                )
-                break
-              default:
-                break
-            }
+    const data = (method: HttpMethod) =>
+      `data${`${hasNotDataQuestion || isConfigRequired ? '' : '?'}: ${
+        typeInfo.data
+          ? `${importName}['${method}']['data']${typeInfo.data.hasQuestion ? ' | null' : ''}`
+          : 'null'
+      }`}`
+    const params = (method: HttpMethod) =>
+      typeInfo.params
+        ? `{ params${
+            typeInfo.params.hasQuestion ? '?' : ''
+          }: ${importName}['${method}']['params'] & { [key: string]: any }} & `
+        : ''
+    const headers = (method: HttpMethod) =>
+      typeInfo.headers
+        ? `{ headers?: ${importName}['${method}']['headers'] & { [key: string]: any }} & `
+        : ''
+    const config = (method: HttpMethod) =>
+      `config${
+        isConfigRequired || (method === 'delete' && hasNotDataQuestion) ? '' : '?'
+      }: ${params(method)}${headers(method)}AxiosRequestConfig`
+    const response = (method: HttpMethod) =>
+      `${method}${typeInfo.response ? `<${importName}['${method}']['response']>` : '<void>'}`
 
-            chanks.push(`${indent}  ${methodName}: ${tmpChanks[0]}
+    switch (methodName) {
+      case 'get':
+        tmpChanks.push(`(${config('get')}) =>`, `client.${response('get')}(\`${newUrl}\`, config)`)
+        break
+      case 'head':
+        tmpChanks.push(
+          `(${config('head')}) =>`,
+          `client.${response('head')}(\`${newUrl}\`, config)`
+        )
+        break
+      case 'post':
+        tmpChanks.push(
+          `(${data('post')}, ${config('post')}) =>`,
+          `client.${response('post')}(\`${newUrl}\`, data, config)`
+        )
+        break
+      case 'put':
+        tmpChanks.push(
+          `(${data('put')}, ${config('put')}) =>`,
+          `client.${response('put')}(\`${newUrl}\`, data, config)`
+        )
+        break
+      case 'patch':
+        tmpChanks.push(
+          `(${data('patch')}, ${config('patch')}) =>`,
+          `client.${response('patch')}(\`${newUrl}\`, data, config)`
+        )
+        break
+      case 'delete':
+        tmpChanks.push(
+          `(${config('delete')}${typeInfo.data ? ` & { ${data('delete')} }` : ''}) =>`,
+          `client.${response('delete')}(\`${newUrl}\`, config)`
+        )
+        break
+      default:
+        break
+    }
+
+    chanks.push(`${indent}  ${methodName}: ${tmpChanks[0]}
 ${indent}    ${tmpChanks[1]},
 ${indent}  $${methodName}: async ${tmpChanks[0]}
 ${indent}    (await ${tmpChanks[1]}).data`)
-          }
-        })
-      }
-    })
-  }
+  })
 
   return chanks.join(',\n')
 }
