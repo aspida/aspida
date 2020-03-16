a59d86e2aae350fa54833bf4fb917bc78fe870b0
m-mitsuhide <genbu2011@live.jp>
2020-03-17 03:00:11 +0900

    feat(openapi)!: delete aspida-mock and rimraf

--- packages/openapi2aspida/src/cli/index.ts
+++ packages/openapi2aspida/src/cli/index.ts
@@ -1,26 +1,24 @@
-import { promisify } from 'util'
 import minimist from 'minimist'
-import rimraf from 'rimraf'
 import getConfig, { Config } from '../getConfig'
 import write from '../writeRouteFile'
-import watch from 'aspida/dist/cli/watchInputDir'
-import { options } from 'aspida/dist/cli'
-import { Build, Watch, CommandToBuild } from './build'
+import { Build, CommandToBuild } from './build'
 import { Command, nullCommand } from './command'
 import { version as versionCommand } from 'aspida/dist/cli/version'
 
-const getBuildCommandFactory = (configs: Config[]) =>
-  CommandToBuild.getFactory(configs, { write, watch, remove: promisify(rimraf) })
+const options: minimist.Opts = {
+  string: ['version', 'config', 'build'],
+  alias: { v: 'version', c: 'config', b: 'build' }
+}
+
+const getBuildCommandFactory = (configs: Config[]) => CommandToBuild.getFactory(configs, { write })
 
 export const run = (args: string[]) => {
   const argv = minimist(args, options)
 
   const commands: Command[] = [
     argv.version !== undefined ? versionCommand : nullCommand,
-    argv.build !== undefined || argv.watch !== undefined
-      ? getBuildCommandFactory(getConfig(argv.config)).create(
-          argv.watch !== undefined ? new Watch() : new Build()
-        )
+    argv.build !== undefined
+      ? getBuildCommandFactory(getConfig(argv.config)).create(new Build())
       : nullCommand
   ]
 
