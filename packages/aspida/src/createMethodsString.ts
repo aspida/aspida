11f26bbf3ff50ef833ea9eade3621765c6dae3b6
m-mitsuhide <genbu2011@live.jp>
2020-04-06 12:21:11 +0900

    chore(deps-dev): bump prettier from 2.0.2 to 2.0.3

--- packages/aspida/src/createMethodsString.ts
+++ packages/aspida/src/createMethodsString.ts
@@ -50,9 +50,10 @@ export default (
       const resMethodName = () =>
         !props.resBody
           ? 'send'
-          : ({ ArrayBuffer: 'arrayBuffer', Blob: 'blob', string: 'text', FormData: 'formData' } as {
-              [key: string]: string
-            })[props.resBody.value] || 'json'
+          : ({ ArrayBuffer: 'arrayBuffer', Blob: 'blob', string: 'text', FormData: 'formData' } as
+              {
+                [key: string]: string
+              })[props.resBody.value] || 'json'
 
       const quotation = newUrl.includes('${') ? '`' : "'"
       const tmpChanks = [
