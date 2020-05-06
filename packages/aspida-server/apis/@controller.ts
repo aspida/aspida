472d1158957c4862c8e9361483ab3d0743254c70
m-mitsuhide <genbu2011@live.jp>
2020-05-07 08:51:23 +0900

    feat(server): add start:server command

--- packages/aspida-server/apis/@controller.ts
+++ packages/aspida-server/apis/@controller.ts
@@ -1,9 +1,15 @@
 import { createController } from 'aspida-server'
 import { Methods } from '.'
 
-export default createController<Methods>({
-  get: v => {
-    return new Promise(resolve => resolve({ status: 200, resBody: v.query }))
+export default createController<Methods>([
+  (req, res, next) => {
+    console.log('Controller level middleware:', req.path)
+    next()
   },
-  post: async () => ({ status: 200, resBody: { id: 1 } })
-})
+  {
+    get: v => {
+      return new Promise(resolve => resolve({ status: 200, resBody: v.query }))
+    },
+    post: async () => ({ status: 200, resBody: { id: 1 } })
+  }
+])
