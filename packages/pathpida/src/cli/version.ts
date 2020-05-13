e8091f9e54bf5cae409cd1da347ab3996351e090
m-mitsuhide <genbu2011@live.jp>
2020-05-13 15:04:54 +0900

    fix: required path of version command

--- packages/pathpida/src/cli/version.ts
+++ packages/pathpida/src/cli/version.ts
@@ -3,6 +3,6 @@ import { Command } from './command'
 
 export const version: Command = {
   exec() {
-    console.log(`v${require(path.join(process.cwd(), 'package.json')).version}`)
+    console.log(`v${require(path.join(__dirname, '../../package.json')).version}`)
   }
 }
