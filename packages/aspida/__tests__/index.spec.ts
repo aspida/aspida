4a0ed190a7fd8c7deb20b94ca5ee5f8a3aaa1d81
m-mitsuhide <genbu2011@live.jp>
2020-05-19 08:17:22 +0900

    feat: write file if changes

--- packages/aspida/__tests__/index.spec.ts
+++ packages/aspida/__tests__/index.spec.ts
@@ -1,13 +1,11 @@
 import fs from 'fs'
 import build from '../src/buildTemplate'
-import write from '../src/writeRouteFile'
 import getConfig from '../src/getConfig'
 
 const basePath = 'packages/aspida'
 
 describe('cli test', () => {
   test('main', () => {
-    const spyLog = jest.spyOn(fs, 'writeFileSync').mockImplementation(x => x)
     const { input, baseURL, trailingSlash } = getConfig(`${basePath}/aspida.config.js`)[0]
     const inputDir = `${basePath}/${input}`
     const inputs = [inputDir, `./${inputDir}`, `./${inputDir}/`, `${inputDir}/`]
@@ -15,11 +13,10 @@ describe('cli test', () => {
     inputs.forEach(inputPath => {
       const resultFilePath = `${inputDir}/$api.ts`
       const result = fs.readFileSync(resultFilePath, 'utf8')
-      write(build({ input: inputPath, baseURL, trailingSlash }))
-      expect(fs.writeFileSync).toHaveBeenLastCalledWith(resultFilePath, result, 'utf8')
-    })
+      const { filePath, text } = build({ input: inputPath, baseURL, trailingSlash })
 
-    spyLog.mockReset()
-    spyLog.mockRestore()
+      expect(filePath).toBe(resultFilePath)
+      expect(text).toBe(result)
+    })
   })
 })
