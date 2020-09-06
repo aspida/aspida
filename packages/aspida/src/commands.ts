f606c5e9ca5dbc3fdef3646f6c44237b9817cdf0
solufa <solufa2020@gmail.com>
2020-09-07 05:48:58 +0900

    fix: delete isFirstBuild option

--- packages/aspida/src/commands.ts
+++ packages/aspida/src/commands.ts
@@ -9,11 +9,12 @@ export const version = (): string => require('../package.json').version
 
 export const build = (config?: Parameters<typeof getConfigs>[0]) =>
   getConfigs(config)
-    .flatMap(c => buildTemplate(c, true))
+    .flatMap(c => buildTemplate(c))
     .forEach(writeRouteFile)
 
 export const watch = (config?: Parameters<typeof getConfigs>[0]) =>
   getConfigs(config).map(c => {
-    buildTemplate(c, true).forEach(writeRouteFile)
-    return watchInputDir(c.input, () => buildTemplate(c, false).forEach(writeRouteFile))
+    buildTemplate(c).forEach(writeRouteFile)
+
+    return watchInputDir(c.input, () => buildTemplate(c).forEach(writeRouteFile))
   })
