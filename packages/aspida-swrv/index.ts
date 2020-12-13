d716ee440ce0011e465a16a8dca8ef0b4e2b7138
solufa <solufa2020@gmail.com>
2020-12-14 03:49:01 +0900

    refactor: clarify types of swr and swrv

--- packages/aspida-swrv/index.ts
+++ packages/aspida-swrv/index.ts
@@ -1,31 +1,27 @@
 import useSWRV, { IConfig } from 'swrv'
 import { IResponse } from 'swrv/dist/types'
 
+type Options<T extends (option: any) => Promise<any>> = Parameters<
+  Parameters<T> extends [Parameters<T>[0]]
+    ? (option: Parameters<T>[0] & IConfig) => void
+    : (option?: Parameters<T>[0] & IConfig) => void
+>
+
+type Res<T extends (option: any) => Promise<any>> = IResponse<
+  ReturnType<T> extends Promise<infer S> ? S : never,
+  any
+>
+
 function useAspidaSWRV<
   T extends Record<string, any> & {
     $get: (option: any) => Promise<any>
     $path: (option?: any) => string
   }
->(
-  api: T,
-  ...option: Parameters<
-    Parameters<T['$get']> extends [Parameters<T['$get']>[0]]
-      ? (option: Parameters<T['$get']>[0] & IConfig) => void
-      : (option?: Parameters<T['$get']>[0] & IConfig) => void
-  >
-): IResponse<ReturnType<T['$get']> extends Promise<infer S> ? S : never, any>
+>(api: T, ...option: Options<T['$get']>): Res<T['$get']>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(
-  api: T,
-  key: U,
-  ...option: Parameters<
-    Parameters<T[U]> extends [Parameters<T[U]>[0]]
-      ? (option: Parameters<T[U]>[0] & IConfig) => void
-      : (option?: Parameters<T[U]>[0] & IConfig) => void
-  >
-): IResponse<ReturnType<T[U]> extends Promise<infer S> ? S : never, any>
+>(api: T, key: U, ...option: Options<T[U]>): Res<T[U]>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
@@ -33,11 +29,7 @@ function useAspidaSWRV<
   const method = typeof key === 'string' ? key : '$get'
   const opt = typeof key === 'string' ? (option as any)[0] : key
 
-  return useSWRV<ReturnType<T[U]> extends Promise<infer S> ? S : never>(
-    `${api.$path(opt)}:${method}`,
-    () => api[method](opt),
-    opt as IConfig
-  )
+  return useSWRV(`${api.$path(opt)}:${method}`, () => api[method](opt), opt)
 }
 
 export default useAspidaSWRV
