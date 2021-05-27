be5449c666feb0b6a7e5e705ffd8bba89e472a72
Kotaro Sugawara <kotarella1110@gmail.com>
2021-05-27 10:04:26 +0900

    Revert "refactor(aspida-swr): improve option arg type"
    This reverts commit 00d737dc4f1d1c10fcb3d4d38b13e4c69c7e70ee.

--- packages/aspida-swr/index.ts
+++ packages/aspida-swr/index.ts
@@ -1,9 +1,20 @@
 import useSWR, { SWRResponse, SWRConfiguration } from 'swr'
 
-type Option<T extends (option: any) => Promise<any>> = Parameters<T>[0] &
-  SWRConfiguration<ResponseData<T>> & {
-    enabled?: boolean
-  }
+type Options<T extends (option: any) => Promise<any>> = Parameters<
+  Parameters<T> extends [Parameters<T>[0]]
+    ? (
+        option: Parameters<T>[0] &
+          SWRConfiguration<ResponseData<T>> & {
+            enabled?: boolean
+          }
+      ) => void
+    : (
+        option?: Parameters<T>[0] &
+          SWRConfiguration<ResponseData<T>> & {
+            enabled?: boolean
+          }
+      ) => void
+>
 
 type ResponseData<T extends (option: any) => Promise<any>> = ReturnType<T> extends Promise<infer S>
   ? S
@@ -14,17 +25,17 @@ function useAspidaSWR<
     $get: (option: any) => Promise<any>
     $path: (option?: any) => string
   }
->(api: T, option?: Option<T['$get']>): SWRResponse<ResponseData<T['$get']>, any>
+>(api: T, ...option: Options<T['$get']>): SWRResponse<ResponseData<T['$get']>, any>
 function useAspidaSWR<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T, key: U, option?: Option<T[U]>): SWRResponse<ResponseData<T[U]>, any>
+>(api: T, key: U, ...option: Options<T[U]>): SWRResponse<ResponseData<T[U]>, any>
 function useAspidaSWR<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T, key: U, option?: Option<T[U]>) {
+>(api: T, key: U, ...option: Parameters<T[U]>) {
   const method = typeof key === 'string' ? key : '$get'
-  const opt = typeof key === 'string' ? (option as any) : key
+  const opt = typeof key === 'string' ? (option as any)[0] : key
 
   return useSWR(
     opt?.enabled === false ? null : [api.$path(opt), method],
