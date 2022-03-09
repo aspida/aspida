714d16546e7bee3983eb600e4fdbc25792eed191
Luma <world@luma.email>
2022-03-09 15:45:32 +0900

    feat(swrv): new option key and fetcher and support reactives for enabled option

--- packages/aspida-swrv/index.ts
+++ packages/aspida-swrv/index.ts
@@ -1,20 +1,29 @@
 import useSWRV, { IConfig } from 'swrv'
 import { IResponse } from 'swrv/dist/types'
 
+type keyType = string | any[] | null
+
+type AspidaSWRVOption<T> = T & {
+  /**
+   * @deprecated
+   * Will be dropped next major release.
+   * Use key and fetcher options instead.
+   *    key: (_opt, getOriginalKey) =>
+   *      enabled ? getOriginalKey() : null,
+   *    fetcher: enabled ? undefined : null
+   */
+  enabled?: boolean
+  key?: (opt: AspidaSWRVOption<T>, getOriginalKey: () => string | null) => keyType
+  fetcher?:
+    | ((f: (opt: AspidaSWRVOption<T>) => any) => (opt: AspidaSWRVOption<T>) => any)
+    | null
+    | undefined
+}
+
 type Options<T extends (option: any) => Promise<any>> = Parameters<
   Parameters<T> extends [Parameters<T>[0]]
-    ? (
-        option: Parameters<T>[0] &
-          IConfig & {
-            enabled?: boolean
-          }
-      ) => void
-    : (
-        option?: Parameters<T>[0] &
-          IConfig & {
-            enabled?: boolean
-          }
-      ) => void
+    ? (option: AspidaSWRVOption<Parameters<T>[0] & Omit<IConfig, 'fetcher'>>) => void
+    : (option?: AspidaSWRVOption<Parameters<T>[0] & Omit<IConfig, 'fetcher'>>) => void
 >
 
 type Res<T extends (option: any) => Promise<any>> = IResponse<
@@ -27,30 +36,48 @@ function useAspidaSWRV<
     $get: (option: any) => Promise<any>
     $path: (option?: any) => string
   }
->(api: T | (() => T | null), ...option: Options<T['$get']>): Res<T['$get']>
+>(api: T | null | (() => T | null), ...option: Options<T['$get']>): Res<T['$get']>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
-  U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T | (() => T | null), key: U, ...option: Options<T[U]>): Res<T[U]>
+  U extends {
+    [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never
+  }[keyof T]
+>(api: T | null | (() => T | null), key: U, ...option: Options<T[U]>): Res<T[U]>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
-  U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T | (() => T | null), key: U, ...option: Parameters<T[U]>) {
-  const method = typeof key === 'string' ? key : '$get'
-  const opt = typeof key === 'string' ? (option as any)[0] : key
+  U extends {
+    [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never
+  }[keyof T]
+>(api: T | null | (() => T | null), maybeKey: U, ...option: Parameters<T[U]>) {
+  if (typeof api === 'function') api = api()
+  const method = typeof maybeKey === 'string' ? maybeKey : '$get'
+  const opt = typeof maybeKey === 'string' ? (option as any)[0] : maybeKey
 
-  return useSWRV(
-    opt?.enabled === false
+  const enabled = opt?.enabled ?? true
+
+  const getOriginalKey = (api => () => {
+    return api && [api.$path(opt), method]
+  })(api)
+
+  const key =
+    opt?.key !== undefined
+      ? typeof opt.key === 'function'
+        ? opt.key(opt, getOriginalKey)
+        : opt.key
+      : enabled
+      ? getOriginalKey()
+      : null
+
+  const fetcherInterv = opt?.fetcher === undefined ? (enabled ? (f: any) => f : null) : opt?.fetcher
+  const fetcher =
+    !api || typeof fetcherInterv !== 'function'
       ? null
-      : typeof api === 'function'
-      ? () => {
-          const client = api()
-          return client && [client.$path(opt), method]
-        }
-      : [api.$path(opt), method],
-    () => (typeof api === 'function' ? api()?.[method](opt) : api[method](opt)),
-    opt
-  )
+      : (api => () => {
+          return fetcherInterv(api[method])(opt)
+        })(api)
+
+  // TODO: Why as any? Wait for https://github.com/Kong/swrv/pull/274.
+  return useSWRV(key, fetcher as any, opt)
 }
 
 export default useAspidaSWRV
