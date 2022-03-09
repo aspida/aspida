4e95a077d49c2a18070e860b37eba6fbfbf787e6
Luma <world@luma.email>
2022-03-09 17:57:06 +0900

    revert(swrv): revert to 1.7.1

--- packages/aspida-swrv/index.ts
+++ packages/aspida-swrv/index.ts
@@ -3,8 +3,18 @@ import { IResponse } from 'swrv/dist/types'
 
 type Options<T extends (option: any) => Promise<any>> = Parameters<
   Parameters<T> extends [Parameters<T>[0]]
-    ? (option: Parameters<T>[0] & IConfig) => void
-    : (option?: Parameters<T>[0] & IConfig) => void
+    ? (
+        option: Parameters<T>[0] &
+          IConfig & {
+            enabled?: boolean
+          }
+      ) => void
+    : (
+        option?: Parameters<T>[0] &
+          IConfig & {
+            enabled?: boolean
+          }
+      ) => void
 >
 
 type Res<T extends (option: any) => Promise<any>> = IResponse<
@@ -17,26 +27,21 @@ function useAspidaSWRV<
     $get: (option: any) => Promise<any>
     $path: (option?: any) => string
   }
->(api: T | (() => T | null), ...option: Options<T['$get']>): Res<T['$get']>
+>(api: T, ...option: Options<T['$get']>): Res<T['$get']>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T | (() => T | null), key: U, ...option: Options<T[U]>): Res<T[U]>
+>(api: T, key: U, ...option: Options<T[U]>): Res<T[U]>
 function useAspidaSWRV<
   T extends Record<string, any> & { $path: (option?: any) => string },
   U extends { [K in keyof T]: T[K] extends (option: any) => Promise<any> ? K : never }[keyof T]
->(api: T | (() => T | null), key: U, ...option: Parameters<T[U]>) {
+>(api: T, key: U, ...option: Parameters<T[U]>) {
   const method = typeof key === 'string' ? key : '$get'
-  const opt = (typeof key === 'string' ? (option as any)[0] : key) as IConfig | undefined
+  const opt = typeof key === 'string' ? (option as any)[0] : key
 
   return useSWRV(
-    typeof api === 'function'
-      ? () => {
-          const client = api()
-          return client && [client.$path(opt), method]
-        }
-      : [api.$path(opt), method],
-    () => (typeof api === 'function' ? api()?.[method](opt) : api[method](opt)),
+    opt?.enabled === false ? null : [api.$path(opt), method],
+    () => api[method](opt),
     opt
   )
 }
